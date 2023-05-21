c10a297799da0091bc56dd8c37e47f52f06ca5ab
umarmaul <umarmaulana31@gmail.com>
2023-05-22 00:37:53 +0700

    mengganti variabel dan menambah tahun

--- linkedlist.c
+++ linkedlist.c
@@ -3,27 +3,29 @@
 #include <string.h>
 
 struct Vehicle {
-    char regNumber[20];
-    char brand[20];
+    char platNomor[20];
+    char merk[20];
     char model[20];
+    char tahun[20];
     struct Vehicle* prev;
     struct Vehicle* next;
 };
 
 struct Vehicle* head = NULL;
 
-struct Vehicle* createVehicle(const char* regNumber, const char* brand, const char* model) {
+struct Vehicle* createVehicle(const char* platNomor, const char* merk, const char* model, const char* tahun) {
     struct Vehicle* newVehicle = (struct Vehicle*)malloc(sizeof(struct Vehicle));
-    strcpy(newVehicle->regNumber, regNumber);
-    strcpy(newVehicle->brand, brand);
+    strcpy(newVehicle->platNomor, platNomor);
+    strcpy(newVehicle->merk, merk);
     strcpy(newVehicle->model, model);
+    strcpy(newVehicle->model, tahun);
     newVehicle->prev = NULL;
     newVehicle->next = NULL;
     return newVehicle;
 }
 
-void insertVehicle(const char* regNumber, const char* brand, const char* model) {
-    struct Vehicle* newVehicle = createVehicle(regNumber, brand, model);
+void insertVehicle(const char* platNomor, const char* merk, const char* model, const char* tahun) {
+    struct Vehicle* newVehicle = createVehicle(platNomor, merk, model, tahun);
     if (head == NULL) {
         head = newVehicle;
     } else {
@@ -34,13 +36,13 @@ void insertVehicle(const char* regNumber, const char* brand, const char* model) 
         current->next = newVehicle;
         newVehicle->prev = current;
     }
-    printf("Vehicle with registration number %s has been added.\n", regNumber);
+    printf("Vehicle with registration number %s has been added.\n", platNomor);
 }
 
-void deleteVehicle(const char* regNumber) {
+void deleteVehicle(const char* platNomor) {
     struct Vehicle* current = head;
     while (current != NULL) {
-        if (strcmp(current->regNumber, regNumber) == 0) {
+        if (strcmp(current->platNomor, platNomor) == 0) {
             if (current->prev != NULL) {
                 current->prev->next = current->next;
             } else {
@@ -50,27 +52,28 @@ void deleteVehicle(const char* regNumber) {
                 current->next->prev = current->prev;
             }
             free(current);
-            printf("Vehicle with registration number %s has been deleted.\n", regNumber);
+            printf("Vehicle with registration number %s has been deleted.\n", platNomor);
             return;
         }
         current = current->next;
     }
-    printf("Vehicle with registration number %s not found.\n", regNumber);
+    printf("Vehicle with registration number %s not found.\n", platNomor);
 }
 
-void searchVehicle(const char* regNumber) {
+void searchVehicle(const char* platNomor) {
     struct Vehicle* current = head;
     while (current != NULL) {
-        if (strcmp(current->regNumber, regNumber) == 0) {
+        if (strcmp(current->platNomor, platNomor) == 0) {
             printf("Vehicle found:\n");
-            printf("Registration Number: %s\n", current->regNumber);
-            printf("Brand: %s\n", current->brand);
+            printf("Registration Number: %s\n", current->platNomor);
+            printf("Brand: %s\n", current->merk);
             printf("Model: %s\n", current->model);
+            printf("Tahun: %s\n", current->tahun);
             return;
         }
         current = current->next;
     }
-    printf("Vehicle with registration number %s not found.\n", regNumber);
+    printf("Vehicle with registration number %s not found.\n", platNomor);
 }
 
 void displayAllVehicles() {
@@ -81,9 +84,10 @@ void displayAllVehicles() {
     }
     printf("All vehicles:\n");
     while (current != NULL) {
-        printf("Registration Number: %s\n", current->regNumber);
-        printf("Brand: %s\n", current->brand);
+        printf("Registration Number: %s\n", current->platNomor);
+        printf("Brand: %s\n", current->merk);
         printf("Model: %s\n", current->model);
+        printf("Tahun: %s\n", current->tahun);
         printf("------------------------\n");
         current = current->next;
     }
@@ -100,9 +104,10 @@ void freeMemory() {
 }
 
 int main() {
-    char regNumber[20];
-    char brand[20];
+    char platNomor[20];
+    char merk[20];
     char model[20];
+    char tahun[20];
     
     insertVehicle("AB1234", "Toyota", "Camry");
     insertVehicle("CD5678", "Honda", "Civic");
@@ -112,10 +117,10 @@ int main() {
  	displayAllVehicles();
 
  	printf("Enter registration number: ");
-    scanf("%s", regNumber);
-    searchVehicle(regNumber);
+    scanf("%s", platNomor);
+    searchVehicle(platNomor);
     
- 	deleteVehicle(regNumber);
+ 	deleteVehicle(platNomor);
 
  	displayAllVehicles();
 
@@ -139,6 +144,4 @@ menunya ada: (pake loop)
 tambahin tahun untuk setiap mobil
 untuk menu search buat agar user bisa memilih search berdasarkan plat atau brand atau model atau tahun
 untuk menu delete by registration number
-
-tes tes
 */
